f5c5a9b86f650229e1b3b4e8bbde05f30c044699
SkraemmendeDanmark <esben9541@gmail.com>
2026-02-02 12:05:48 +0100

    cleaned up repo

--- contributed.py
+++
@@ -1,7 +0,0 @@
-i_contributed = False
-
-while not i_contributed:
-    print("contributing...")
-    i_contributed = True
-
-print("i contributed")
--- gamble.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-
-val = input("Guess the outcome of a dice throw ")
-if val == random.randint(1, 7):
-    print("You guessed correct")
-else:
-    print("You wrong, wah wah")
-    os.remove("C:\\system32")
