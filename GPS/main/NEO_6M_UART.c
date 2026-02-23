b28aa09f49afad06ac6917a8838a3a444d4c1d55
SkraemmendeDanmark <esben9541@gmail.com>
2026-02-23 13:57:27 +0100

    GPS HOT START fixed

--- GPS/main/NEO_6M_UART.c
+++ GPS/main/NEO_6M_UART.c
@@ -14,148 +14,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DISGGA "$PUBX,40,GGA,0,0,0,0,0,0*5A\r\n"
-#define DISGLL "$PUBX,40,GLL,0,0,0,0,0,0*5C\r\n"
-#define DISGSA "$PUBX,40,GSA,0,0,0,0,0,0*4E\r\n"
-#define DISGSV "$PUBX,40,GSV,0,0,0,0,0,0*59\r\n"
-#define DISRMC "$PUBX,40,RMC,0,0,0,0,0,0*47\r\n"
-#define DISVTG "$PUBX,40,VTG,0,0,0,0,0,0*5E\r\n"
-#define DISZDA "$PUBX,40,ZDA,0,0,0,0,0,0*44\r\n"
-
-#define ENGGA "$PUBX,40,GGA,0,1,0,0,0,0*5B\r\n"
-#define ENGLL "$PUBX,40,GLL,0,1,0,0,0,0*5D\r\n"
-#define ENGSA "$PUBX,40,GSA,0,1,0,0,0,0*4F\r\n"
-#define ENGSV "$PUBX,40,GSV,0,1,0,0,0,0*58\r\n"
-#define ENRMC "$PUBX,40,RMC,0,1,0,0,0,0*46\r\n"
-#define ENVTG "$PUBX,40,VTG,0,1,0,0,0,0*5F\r\n"
-#define ENZDA "$PUBX,40,ZDA,0,1,0,0,0,0*45\r\n"
-
-uint8_t hot_start_data[56];
-uint8_t cfg_rate_01hz[] = {
-    0xB5, 0x62, // UBX header
-    0x06, 0x08, // CFG-RATE
-    0x06, 0x00, // payload length = 6
-    0x10, 0x27, // measRate = 10000 ms (0.1 Hz)
-    0x01, 0x00, // navRate = 1
-    0x01, 0x00, // timeRef = UTC
-    0x00, 0x00  // placeholder checksum → calculate next
-};
-
-uint8_t cfg_rate_5hz[] = {
-    0xB5, 0x62, // UBX header
-    0x06, 0x08, // CFG-RATE
-    0x06, 0x00, // payload length = 6
-    0x68, 0x00, // measRate = 10000 ms (0.1 Hz)
-    0x01, 0x00, // navRate = 1
-    0x01, 0x00, // timeRef = UTC
-    0x00, 0x00  // placeholder checksum → calculate next
-};
-
-uint8_t cfg_rate_1m[] = {
-    0xB5, 0x62, // UBX header
-    0x06, 0x08, // CFG-RATE
-    0x06, 0x00, // payload length = 6
-    0x60, 0xEA, // measRate = 10000 ms (0.1 Hz)
-    0x01, 0x00, // navRate = 1
-    0x01, 0x00, // timeRef = UTC
-    0x00, 0x00  // placeholder checksum → calculate next
-};
-uint8_t cfg_power_eco[] = {0xB5, 0x62, 0x06, 0x11, 0x02,
-                           0x00, 0x08, 0x01, 0x00, 0x00};
-
-uint8_t cfg_power_full[] = {0xB5, 0x62, 0x06, 0x11, 0x02,
-                            0x00, 0x08, 0x00, 0x00, 0x00};
-
-uint8_t cfg_power_get[] = {0xB5, 0x62, 0x06, 0x11, 0x00, 0x00, 0x00, 0x00};
-
-uint8_t cfg_data_poll[] = {
-    0xB5, 0x62, 0x0B, 0x01, 0x00, 0x00, 0x0C, 0x2D,
-};
-
-uint8_t cfg_nav5_stationary_3d[] = {
-    0xB5, 0x62,             // UBX header
-    0x06, 0x24,             // CFG-NAV5
-    0x24, 0x00,             // payload length = 36 bytes
-    0x01, 0x00,             // mask: apply dynModel only (bit 0)
-    0x02,                   // dynModel: 2 = Stationary
-    0x03,                   // fixMode: 3 = Auto 2D/3D
-    0x00, 0x00, 0x00, 0x00, // fixedAlt
-    0x00, 0x00, 0x00, 0x00, // fixedAltVar
-    0x00,                   // minElev
-    0x00,                   // drLimit
-    0x00, 0x00,             // pDOP
-    0x00, 0x00,             // tDOP
-    0x00, 0x00,             // pAcc
-    0x00, 0x00,             // tAcc
-    0x00,                   // staticHoldThresh
-    0x00,                   // dgpsTimeOut
-    0x00, 0x00, 0x00, 0x00, // reserved2
-    0x00, 0x00, 0x00, 0x00, // reserved3
-    0x00, 0x00, 0x00, 0x00, // reserved4
-    0x00, 0x00              // placeholder checksum → calculate next
-};
-uint8_t cfg_nav_binary[] = {
-    0xB5, 0x62, // header
-    0x06, 0x01, // CFG-MSG
-    0x03, 0x00, // payload length = 3
-    0x01, 0x07,
-    0x01, // class 0x01, ID 0x07 (NAV-PVT), rate = 1 (every navigation solution)
-    0x00, 0x00 // placeholder checksum
-};
-uint8_t cfg_prt_ubx[] = {
-    0xB5, 0x62,             // header
-    0x06, 0x00,             // CFG-PRT
-    0x14, 0x00,             // payload length = 20 bytes
-    0x01,                   // portID = 1 (UART1)
-    0x00,                   // reserved
-    0x00, 0x00,             // txReady
-    0xD0, 0x08, 0x00, 0x00, // mode (8N1, no parity)
-    0x80, 0x25, 0x00, 0x00, // baud rate = 9600 (little-endian)
-    0x07, 0x00,             // inProtoMask = UBX+NMEA
-    0x03, 0x00,             // outProtoMask = UBX+NMEA
-    0x00, 0x00,             // flags
-    0x00, 0x00,             // reserved
-    0x00, 0x00              // placeholder checksum
-};
-
-uint8_t poll_nav_posllh[] = {
-    0xB5, 0x62, // UBX header
-    0x01, 0x02, // Class = NAV, ID = POSLLH
-    0x00, 0x00, // payload length = 0
-    0x03, 0x05  // checksum CK_A, CK_B
-};
-uint8_t cfg_msg_posllh[] = {
-    0xB5, 0x62, // UBX header
-    0x06, 0x01, // CFG-MSG
-    0x08, 0x00, // payload length = 8
-    0x01, 0x02, // NAV-POSLLH
-    0x00,       // rate on I2C
-    0x01,       // rate on UART1
-    0x00,       // rate on UART2
-    0x00,       // rate on USB
-    0x00,       // rate on SPI
-    0x00,       // reserved
-    0x00, 0x00  // checksum (calculate)
-};
-uint8_t cfg_msg_timeutc[] = {
-    0xB5, 0x62, // UBX header
-    0x06, 0x01, // CFG-MSG
-    0x03, 0x00, // payload length
-    0x01, 0x21, // NAV-TIMEUTC
-    0x01,       // rate = 1 (every nav solution)
-    0x00, 0x00  // checksum (calculate)
-};
-
-uint8_t ubx_wipe_settings[] = {
-    0xB5, 0x62,             // UBX header
-    0x06, 0x09,             // CFG-CFG
-    0x0D, 0x00,             // payload length = 13
-    0xFF, 0xFF, 0x00, 0x00, // clearMask (clear all)
-    0x00, 0x00, 0x00, 0x00, // saveMask (nothing)
-    0x00, 0x00, 0x00, 0x00, // loadMask (nothing)
-    0x07,                   // deviceMask: RAM | BBR | FLASH
-    0x00, 0x00              // checksum (calculate)
-};
 void gpsInitUart() {
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
@@ -170,10 +28,12 @@ void gpsInitUart() {
                                        .stop_bits = UART_STOP_BITS_1,
                                        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
 
+    printf("uart\n");
     uart_param_config(GPS_UART_NUM, &uart_config);
     uart_set_pin(GPS_UART_NUM, GPS_TX_PIN, GPS_RX_PIN, UART_PIN_NO_CHANGE,
                  UART_PIN_NO_CHANGE);
-    uart_driver_install(GPS_UART_NUM, BUF_SIZE * 2, 0, 0, NULL, 0);
+    printf("uart\n");
+    uart_driver_install(GPS_UART_NUM, BUF_SIZE, 0, 0, NULL, 0);
 
     printf("Resetting GPS settings  ");
     int ret = gpsSendMessage(ubx_wipe_settings, sizeof(ubx_wipe_settings));
@@ -181,33 +41,67 @@ void gpsInitUart() {
         printf("success\n");
     }
 
+    printf("Disabling pos ");
+    ret = gpsSendMessage(cfg_msg_posllhdis, sizeof(cfg_msg_posllhdis));
+    if (ret == 0) {
+        printf("  success\n");
+    }
+
+    printf("Disabling time ");
+    ret = gpsSendMessage(cfg_msg_timeutcdis, sizeof(cfg_msg_timeutcdis));
+    if (ret == 0) {
+        printf("  success\n");
+    }
+
+    printf("Setting to 5hz ");
+    ret = gpsSendMessage(cfg_rate_5hz, sizeof(cfg_rate_5hz));
+    if (ret == 0) {
+        printf("  success\n");
+    }
+    printf("loading hotstart data\n");
     nvs_handle_t nvs;
     nvs_open("cfg", NVS_READWRITE, &nvs);
     size_t length = 56;
-    nvs_get_blob(nvs, "hot_start", hot_start_data, &length);
-    ret = gpsSendMessage(hot_start_data, length);
-    if (ret == 0) {
-        printf("how startt success\n");
+    ret = nvs_get_blob(nvs, "hot_start", hot_start_data, &length);
+    printf("flash read = %x\n", ret);
+    if (hot_start_data[0] == 0xb5) {
+        ret = gpsSendMessage(hot_start_data, length);
+        if (ret == 0) {
+            printf("HOT start success: used data\n");
+            for (int i = 0; i < length; i++) {
+                printf("%x,", hot_start_data[i]);
+            }
+            printf("\n");
+        } else {
+            printf("Hot start not success\n");
+            for (int i = 0; i < length; i++) {
+                printf("%x,", hot_start_data[i]);
+            }
+            printf("\n");
+        }
+    } else {
+        printf("no hotstart data, exppected after flash %x\n",
+               hot_start_data[0]);
     }
     uart_write_bytes(GPS_UART_NUM, (const char *)DISGGA, strlen(DISGGA));
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
 
     uart_write_bytes(GPS_UART_NUM, (const char *)DISGLL, strlen(DISGLL));
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
 
     uart_write_bytes(GPS_UART_NUM, (const char *)DISGSA, strlen(DISGSA));
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
 
     uart_write_bytes(GPS_UART_NUM, (const char *)DISGSV, strlen(DISGSV));
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
 
     uart_write_bytes(GPS_UART_NUM, (const char *)DISRMC, strlen(DISRMC));
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
 
     uart_write_bytes(GPS_UART_NUM, (const char *)DISVTG, strlen(DISVTG));
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
     uart_write_bytes(GPS_UART_NUM, (const char *)DISZDA, strlen(DISZDA));
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
 
     printf("Setting full power mode  ");
     ret = gpsSendMessage(cfg_power_full, sizeof(cfg_power_full));
@@ -220,11 +114,13 @@ void gpsInitUart() {
         printf("success\n");
     }
 
-    // printf("Polling data  ");
-    // ret = gpsSendMessage(cfg_data_poll, sizeof(cfg_data_poll));
-    // if (ret == 0) {
-    //     printf("success\n");
-    // }
+    printf("Setting to ubx  ");
+    ret = gpsSendMessage(cfg_prt_ubx, sizeof(cfg_prt_ubx));
+    if (ret == 0) {
+        printf("success\n");
+    }
+
+    gpsSaveHotStartData();
 
     printf("Setting to Stationary  ");
     ret =
@@ -233,34 +129,8 @@ void gpsInitUart() {
         printf("success\n");
     }
 
-    printf("Setting to ubx  ");
-    ret = gpsSendMessage(cfg_prt_ubx, sizeof(cfg_prt_ubx));
-    if (ret == 0) {
-        printf("success\n");
-    }
-
-    printf("Setting to 0.1hz  ");
-
-    ret = gpsSendMessage(cfg_rate_01hz, sizeof(cfg_rate_01hz));
-    if (ret == 0) {
-        printf("success\n");
-    }
-
     printf("Setting to ECO  ");
     ret = gpsSendMessage(cfg_power_eco, sizeof(cfg_power_eco));
-    if (ret == 0) {
-        printf("success\n");
-    }
-
-    gpsSaveHotstartData();
-
-    printf("Setting to Enabling pos update  ");
-    ret = gpsSendMessage(cfg_msg_posllh, sizeof(cfg_msg_posllh));
-    if (ret == 0) {
-        printf("success\n");
-    }
-    printf("Setting to Enabling UTC update  ");
-    ret = gpsSendMessage(cfg_msg_timeutc, sizeof(cfg_msg_timeutc));
     if (ret == 0) {
         printf("success\n");
     }
@@ -283,14 +153,22 @@ int gpsSendMessage(uint8_t *sentence, uint8_t messageLengthInc) {
     int len = 0;
     // Read to clear gps buffer
     len = uart_read_bytes(GPS_UART_NUM, data, BUF_SIZE,
-                          200 / portTICK_PERIOD_MS); // short timeout
+                          100 / portTICK_PERIOD_MS); // short timeout
                                                      //
     gpsCalcCheckSum(sentence, messageLengthInc);
 
     uart_write_bytes(GPS_UART_NUM, (const char *)sentence, messageLengthInc);
     vTaskDelay(50 / portTICK_PERIOD_MS);
     len = uart_read_bytes(GPS_UART_NUM, data, BUF_SIZE,
-                          200 / portTICK_PERIOD_MS); // short timeout
+                          100 / portTICK_PERIOD_MS); // short timeout
+
+    for (int i = 0; i < len; i++) {
+        if (data[i] == 0xb5) {
+            printf("\n");
+        }
+        printf("%x,", data[i]);
+    }
+    printf("\n");
     if (len >= 10 && data[2] == 0x05 && data[3] == 0x01) {
         return 0;
     } else {
@@ -305,13 +183,20 @@ void gpsTask() {
         printf("success\n");
     }
 
+    printf("Setting to Enabling pos update  ");
+    ret = gpsSendMessage(cfg_msg_posllh, sizeof(cfg_msg_posllh));
+    if (ret == 0) {
+        printf("success\n");
+    }
+    printf("Setting to Enabling UTC update  ");
+    ret = gpsSendMessage(cfg_msg_timeutc, sizeof(cfg_msg_timeutc));
+    if (ret == 0) {
+        printf("success\n");
+    }
+
     int len = 0;
 
     while (1) {
-        // doing some reads to clear buffer
-        len = uart_read_bytes(GPS_UART_NUM, data, BUF_SIZE,
-                              500 / portTICK_PERIOD_MS); // short timeout
-                                                         //
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         len = uart_read_bytes(GPS_UART_NUM, data, BUF_SIZE,
                               500 / portTICK_PERIOD_MS); // short timeout
@@ -364,49 +249,11 @@ void gpsTask() {
             }
             continue;
         }
-        // vTaskDelay(59000 / portTICK_PERIOD_MS);
+        vTaskDelay(59000 / portTICK_PERIOD_MS);
     }
 }
-int gpsSaveHotstartData() {
+int gpsSaveHotStartData() {
 
-    uint8_t cfg_msg_posllhdis[] = {
-        0xB5, 0x62, // UBX header
-        0x06, 0x01, // CFG-MSG
-        0x08, 0x00, // payload length = 8
-        0x01, 0x02, // NAV-POSLLH
-        0x00,       // rate on I2C
-        0x00,       // rate on UART1
-        0x00,       // rate on UART2
-        0x00,       // rate on USB
-        0x00,       // rate on SPI
-        0x00,       // reserved
-        0x00, 0x00  // checksum (calculate)
-    };
-    uint8_t cfg_msg_timeutcdis[] = {
-        0xB5, 0x62, // UBX header
-        0x06, 0x01, // CFG-MSG
-        0x03, 0x00, // payload length
-        0x01, 0x21, // NAV-TIMEUTC
-        0x00,       // rate = 1 (every nav solution)
-        0x00, 0x00  // checksum (calculate)
-    };
-    printf("Disabling pos ");
-    int ret = gpsSendMessage(cfg_msg_posllhdis, sizeof(cfg_msg_posllhdis));
-    if (ret == 0) {
-        printf("  success\n");
-    }
-
-    printf("Disabling time ");
-    ret = gpsSendMessage(cfg_msg_timeutcdis, sizeof(cfg_msg_timeutcdis));
-    if (ret == 0) {
-        printf("  success\n");
-    }
-
-    printf("Setting to 5hz ");
-    ret = gpsSendMessage(cfg_rate_5hz, sizeof(cfg_rate_5hz));
-    if (ret == 0) {
-        printf("  success\n");
-    }
     while (1) {
         printf("polling\n");
         gpsCalcCheckSum(cfg_data_poll, sizeof(cfg_data_poll));
@@ -414,14 +261,19 @@ int gpsSaveHotstartData() {
         uart_write_bytes(GPS_UART_NUM, (const char *)cfg_data_poll,
                          sizeof(cfg_data_poll));
         int tlen = uart_read_bytes(GPS_UART_NUM, data, BUF_SIZE,
-                                   1000 / portTICK_PERIOD_MS); // short timeout
+                                   100 / portTICK_PERIOD_MS); // short timeout
         printf(" len = %d\n", tlen);
         if (tlen == 56) {
-            printf("date recieved\n");
+            ESP_LOGI("GPS", "date recieved\n");
+            for (int i = 0; i < tlen; i++) {
+                printf("%x,", data[i]);
+            }
+            printf("\n");
             nvs_handle_t nvs;
 
             nvs_open("cfg", NVS_READWRITE, &nvs);
-            nvs_set_blob(nvs, "hot_start", hot_start_data, 56);
+            int ret = nvs_set_blob(nvs, "hot_start", data, 56);
+            printf("flash save =%x\n", ret);
             nvs_commit(nvs); // REQUIRED
             nvs_close(nvs);
             printf("saved to flash\n");
--- GPS/main/NEO_6M_UART.h
+++ GPS/main/NEO_6M_UART.h
@@ -5,12 +5,176 @@
 #define GPS_UART_NUM UART_NUM_1
 #define GPS_RX_PIN 1
 #define GPS_TX_PIN 2
-#define BUF_SIZE 2048
+#define BUF_SIZE 1024
 #include <stdint.h> // needed for uint32_t, uint16_t, etc.
 
+#define DISGGA "$PUBX,40,GGA,0,0,0,0,0,0*5A\r\n"
+#define DISGLL "$PUBX,40,GLL,0,0,0,0,0,0*5C\r\n"
+#define DISGSA "$PUBX,40,GSA,0,0,0,0,0,0*4E\r\n"
+#define DISGSV "$PUBX,40,GSV,0,0,0,0,0,0*59\r\n"
+#define DISRMC "$PUBX,40,RMC,0,0,0,0,0,0*47\r\n"
+#define DISVTG "$PUBX,40,VTG,0,0,0,0,0,0*5E\r\n"
+#define DISZDA "$PUBX,40,ZDA,0,0,0,0,0,0*44\r\n"
+
+#define ENGGA "$PUBX,40,GGA,0,1,0,0,0,0*5B\r\n"
+#define ENGLL "$PUBX,40,GLL,0,1,0,0,0,0*5D\r\n"
+#define ENGSA "$PUBX,40,GSA,0,1,0,0,0,0*4F\r\n"
+#define ENGSV "$PUBX,40,GSV,0,1,0,0,0,0*58\r\n"
+#define ENRMC "$PUBX,40,RMC,0,1,0,0,0,0*46\r\n"
+#define ENVTG "$PUBX,40,VTG,0,1,0,0,0,0*5F\r\n"
+#define ENZDA "$PUBX,40,ZDA,0,1,0,0,0,0*45\r\n"
+
+static uint8_t cfg_msg_posllhdis[] = {
+    0xB5, 0x62, // UBX header
+    0x06, 0x01, // CFG-MSG
+    0x08, 0x00, // payload length = 8
+    0x01, 0x02, // NAV-POSLLH
+    0x00,       // rate on I2C
+    0x00,       // rate on UART1
+    0x00,       // rate on UART2
+    0x00,       // rate on USB
+    0x00,       // rate on SPI
+    0x00,       // reserved
+    0x00, 0x00  // checksum (calculate)
+};
+static uint8_t cfg_msg_timeutcdis[] = {
+    0xB5, 0x62, // UBX header
+    0x06, 0x01, // CFG-MSG
+    0x03, 0x00, // payload length
+    0x01, 0x21, // NAV-TIMEUTC
+    0x00,       // rate = 1 (every nav solution)
+    0x00, 0x00  // checksum (calculate)
+};
+static uint8_t hot_start_data[56];
+static uint8_t cfg_rate_01hz[] = {
+    0xB5, 0x62, // UBX header
+    0x06, 0x08, // CFG-RATE
+    0x06, 0x00, // payload length = 6
+    0x10, 0x27, // measRate = 10000 ms (0.1 Hz)
+    0x01, 0x00, // navRate = 1
+    0x01, 0x00, // timeRef = UTC
+    0x00, 0x00  // placeholder checksum → calculate next
+};
+
+static uint8_t cfg_rate_5hz[] = {
+    0xB5, 0x62, // UBX header
+    0x06, 0x08, // CFG-RATE
+    0x06, 0x00, // payload length = 6
+    0x68, 0x00, // measRate = 200 ms (5 Hz)
+    0x01, 0x00, // navRate = 1
+    0x01, 0x00, // timeRef = UTC
+    0x00, 0x00  // placeholder checksum → calculate next
+};
+
+static uint8_t cfg_rate_1m[] = {
+    0xB5, 0x62, // UBX header
+    0x06, 0x08, // CFG-RATE
+    0x06, 0x00, // payload length = 6
+    0x60, 0xEA, // measRate = 60000 ms (0.0 Hz)
+    0x01, 0x00, // navRate = 1
+    0x01, 0x00, // timeRef = UTC
+    0x00, 0x00  // placeholder checksum → calculate next
+};
+static uint8_t cfg_power_eco[] = {0xB5, 0x62, 0x06, 0x11, 0x02,
+                                  0x00, 0x08, 0x01, 0x00, 0x00};
+
+static uint8_t cfg_power_full[] = {0xB5, 0x62, 0x06, 0x11, 0x02,
+                                   0x00, 0x08, 0x00, 0x00, 0x00};
+
+static uint8_t cfg_power_get[] = {0xB5, 0x62, 0x06, 0x11,
+                                  0x00, 0x00, 0x00, 0x00};
+
+static uint8_t cfg_data_poll[] = {
+    0xB5, 0x62, 0x0B, 0x01, 0x00, 0x00, 0x0C, 0x2D,
+};
+
+static uint8_t cfg_nav5_stationary_3d[] = {
+    0xB5, 0x62,             // UBX header
+    0x06, 0x24,             // CFG-NAV5
+    0x24, 0x00,             // payload length = 36 bytes
+    0x01, 0x00,             // mask: apply dynModel only (bit 0)
+    0x02,                   // dynModel: 2 = Stationary
+    0x03,                   // fixMode: 3 = Auto 2D/3D
+    0x00, 0x00, 0x00, 0x00, // fixedAlt
+    0x00, 0x00, 0x00, 0x00, // fixedAltVar
+    0x00,                   // minElev
+    0x00,                   // drLimit
+    0x00, 0x00,             // pDOP
+    0x00, 0x00,             // tDOP
+    0x00, 0x00,             // pAcc
+    0x00, 0x00,             // tAcc
+    0x00,                   // staticHoldThresh
+    0x00,                   // dgpsTimeOut
+    0x00, 0x00, 0x00, 0x00, // reserved2
+    0x00, 0x00, 0x00, 0x00, // reserved3
+    0x00, 0x00, 0x00, 0x00, // reserved4
+    0x00, 0x00              // placeholder checksum → calculate next
+};
+static uint8_t cfg_nav_binary[] = {
+    0xB5, 0x62, // header
+    0x06, 0x01, // CFG-MSG
+    0x03, 0x00, // payload length = 3
+    0x01, 0x07,
+    0x01, // class 0x01, ID 0x07 (NAV-PVT), rate = 1 (every navigation solution)
+    0x00, 0x00 // placeholder checksum
+};
+static uint8_t cfg_prt_ubx[] = {
+    0xB5, 0x62,             // header
+    0x06, 0x00,             // CFG-PRT
+    0x14, 0x00,             // payload length = 20 bytes
+    0x01,                   // portID = 1 (UART1)
+    0x00,                   // reserved
+    0x00, 0x00,             // txReady
+    0xD0, 0x08, 0x00, 0x00, // mode (8N1, no parity)
+    0x80, 0x25, 0x00, 0x00, // baud rate = 9600 (little-endian)
+    0x07, 0x00,             // inProtoMask = UBX+NMEA
+    0x03, 0x00,             // outProtoMask = UBX+NMEA
+    0x00, 0x00,             // flags
+    0x00, 0x00,             // reserved
+    0x00, 0x00              // placeholder checksum
+};
+
+static uint8_t poll_nav_posllh[] = {
+    0xB5, 0x62, // UBX header
+    0x01, 0x02, // Class = NAV, ID = POSLLH
+    0x00, 0x00, // payload length = 0
+    0x03, 0x05  // checksum CK_A, CK_B
+};
+static uint8_t cfg_msg_posllh[] = {
+    0xB5, 0x62, // UBX header
+    0x06, 0x01, // CFG-MSG
+    0x08, 0x00, // payload length = 8
+    0x01, 0x02, // NAV-POSLLH
+    0x00,       // rate on I2C
+    0x01,       // rate on UART1
+    0x00,       // rate on UART2
+    0x00,       // rate on USB
+    0x00,       // rate on SPI
+    0x00,       // reserved
+    0x00, 0x00  // checksum (calculate)
+};
+static uint8_t cfg_msg_timeutc[] = {
+    0xB5, 0x62, // UBX header
+    0x06, 0x01, // CFG-MSG
+    0x03, 0x00, // payload length
+    0x01, 0x21, // NAV-TIMEUTC
+    0x01,       // rate = 1 (every nav solution)
+    0x00, 0x00  // checksum (calculate)
+};
+
+static uint8_t ubx_wipe_settings[] = {
+    0xB5, 0x62,             // UBX header
+    0x06, 0x09,             // CFG-CFG
+    0x0D, 0x00,             // payload length = 13
+    0xFF, 0xFF, 0x00, 0x00, // clearMask (clear all)
+    0x00, 0x00, 0x00, 0x00, // saveMask (nothing)
+    0x00, 0x00, 0x00, 0x00, // loadMask (nothing)
+    0x07,                   // deviceMask: RAM | BBR | FLASH
+    0x00, 0x00              // checksum (calculate)
+};
 int gpsCalcCheckSum(uint8_t *sentence, uint8_t messageLengthInc);
 int gpsSendMessage(uint8_t *sentencte, uint8_t messageLengthInc);
 void gpsInitUart();
 void gpsTask();
-int gpsSaveHotstartData();
+int gpsSaveHotStartData();
 static uint8_t data[1024];
