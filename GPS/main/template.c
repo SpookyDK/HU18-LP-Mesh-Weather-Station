318ee7ddc8a95ca1e7badf0eb227f942a8b2b8e5
SkraemmendeDanmark <esben9541@gmail.com>
2026-02-23 14:38:32 +0100

    Added perf monitoringt

--- GPS/main/template.c
+++ GPS/main/template.c
@@ -1,10 +1,26 @@
 #include "NEO_6M_UART.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/FreeRTOSConfig.h"
+#include "freertos/task.h"
 #include <driver/uart.h>
 #include <stdint.h>
 #include <stdio.h>
+void print_runtime_stats(void) {
+    char buffer[1024];
 
+    vTaskGetRunTimeStats(buffer);
+
+    printf("Task Name\tRuntime\tCPU %%\n");
+    printf("%s\n", buffer);
+}
 void app_main(void) {
+    // xTaskCreate(gpsInitUart, "initGPS", 4096, NULL, 0, NULL);
     gpsInitUart();
-    gpsTask();
+    xTaskCreate(gpsTask, "GPSTask", 2048, NULL, 0, NULL);
+    // gpsTask();
+    while (1) {
+        print_runtime_stats();
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
 }
---
+++ vejleder.md
@@ -0,0 +1,18 @@
+3. marts,  skriv gerne.
+Han har travlt indtil 3.marts, og i skal bare skrive vis han glemmer os.
+
+Han synes det med at argumentere for Køb også en service aftale er fin løsning.
+
+Glad for at vi har defineret landmands størrelse
+
+Nemmere end PBL spaden
+ 
+Han kender en hobby landmands
+
+Helt fint med ikke at have den store kontakt med landmændende.  Må gerne bruges som lidt opbakning / løbende kontakt.
+
+Snak med kenneth om at bestille ting
+
+Budget er nok fint 570kr
+
+Han har vind og regn spand vis vi synes det kan bruges.
