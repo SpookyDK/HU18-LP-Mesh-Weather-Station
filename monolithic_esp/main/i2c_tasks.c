cc3c6e15340fc4d4b6f6cf04465a2f1c81d33de2
zaddmc <davidbk927@gmail.com>
2026-03-06 13:05:04 +0100

    driver issue

--- monolithic_esp/main/i2c_tasks.c
+++ monolithic_esp/main/i2c_tasks.c
@@ -1,19 +1,29 @@
-#include "accel.h"
 #include "bmx280.h"
 #include "driver/i2c.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/projdefs.h"
 #include "freertos/task.h"
-#include <math.h>
+#include "i2c_tasks.h"
+#include "tsl2591.h"
 #include <stdint.h>
-#include <stdio.h>
+
+#define I2C_MASTER_SCL_IO 11
+#define I2C_MASTER_SDA_IO 10
+#define I2C_MASTER_NUM I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ 100000
 
 static const char *TAG = "accel";
 
 static bmx280_t *sensor;
 
-static void init_bmp280(void) {
+static bool is_i2c_init = false;
+
+static void init_i2c(void) {
+    if (is_i2c_init) {
+        return;
+    }
 
     i2c_config_t i2c_conf = {
         .mode = I2C_MODE_MASTER,
@@ -26,6 +36,12 @@ static void init_bmp280(void) {
 
     ESP_ERROR_CHECK(i2c_param_config(I2C_MASTER_NUM, &i2c_conf));
     ESP_ERROR_CHECK(i2c_driver_install(I2C_MASTER_NUM, i2c_conf.mode, 0, 0, 0));
+    is_i2c_init = true;
+}
+
+static void init_bmp280(void) {
+    // Ensure I2C is started
+    init_i2c();
 
     // Initialize BMP280
     sensor = bmx280_create(I2C_MASTER_NUM);
@@ -68,4 +84,35 @@ void accel_task(void *duty_cycle_ms) {
     bmx280_close(sensor);
     ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
     ESP_LOGW(TAG, "Leaving Accel Task");
+}
+
+static tsl2591_t light_sensor_dev;
+static void init_light_sensor(void) {
+    // Ensure I2C is stated
+    init_i2c();
+
+    ESP_ERROR_CHECK(tsl2591_init_desc(&light_sensor_dev, I2C_MASTER_NUM, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO));
+    ESP_ERROR_CHECK(tsl2591_init(&light_sensor_dev));
+
+    ESP_ERROR_CHECK(tsl2591_set_power_status(&light_sensor_dev, TSL2591_POWER_ON));
+    ESP_ERROR_CHECK(tsl2591_set_als_status(&light_sensor_dev, TSL2591_ALS_ON));
+
+    ESP_ERROR_CHECK(tsl2591_set_gain(&light_sensor_dev, TSL2591_GAIN_MEDIUM));
+    ESP_ERROR_CHECK(tsl2591_set_integration_time(&light_sensor_dev, TSL2591_INTEGRATION_300MS));
+}
+
+void light_sensor_task(void *duty_cycle_ms) {
+    init_light_sensor();
+
+    float lux;
+    esp_err_t res;
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        if ((res = tsl2591_get_lux(&light_sensor_dev, &lux)) == ESP_OK) {
+            ESP_LOGI("Light", "Lux reading: %f", lux);
+        } else {
+            ESP_LOGW("Light", "Cound not read Lux value: %d", res);
+        }
+    }
 }
--- monolithic_esp/main/main.c
+++ monolithic_esp/main/main.c
@@ -4,7 +4,7 @@
 
 #define DUTY_CYCLES_MS 30000
 #include "NEO_6M_UART.h"
-#include "accel.h"
+#include "i2c_tasks.h"
 #include "moist_soil.h"
 #include "tempeture.h"
 
@@ -21,14 +21,17 @@ void print_runtime_stats(void) {
 
 void app_main(void) {
 
-    ESP_LOGI(TAG, "Starting Tempeture Task");
-    xTaskCreate(temp_task, "tempTask", 4096, (void *)DUTY_CYCLES_MS, 0, NULL);
+    /* ESP_LOGI(TAG, "Starting Tempeture Task");
+    xTaskCreate(temp_task, "tempTask", 4096, (void *)DUTY_CYCLES_MS, 0, NULL); */
 
-    ESP_LOGI(TAG, "Starting Soil Moisture Reading Task");
-    xTaskCreate(moist_task, "moistTask", 4096, (void *)DUTY_CYCLES_MS, 0, NULL);
+    /* ESP_LOGI(TAG, "Starting Soil Moisture Reading Task");
+    xTaskCreate(moist_task, "moistTask", 4096, (void *)DUTY_CYCLES_MS, 0, NULL); */
 
     ESP_LOGI(TAG, "Starting Accelerometer Task");
     xTaskCreate(accel_task, "accelTask", 4096, (void *)DUTY_CYCLES_MS, 0, NULL);
+
+    ESP_LOGI(TAG, "Starting Light Sensor Task");
+    xTaskCreate(accel_task, "lightTask", 4096, (void *)DUTY_CYCLES_MS, 0, NULL);
 
     /* ESP_LOGI(TAG, "Starting GPS Task");
     xTaskCreate(gpsTask, "gpsTask", 4096, NULL, 0, NULL); */
