fcc620b292dfca2a69c7a62d2d279d9d04f0a177
Esben Jørgensen <105819593+SpookyDK@users.noreply.github.com>
2026-03-15 12:01:42 +0000

    I fucked up the pins

--- monolithic_esp/main/tempeture.h
+++ monolithic_esp/main/tempeture.h
@@ -3,10 +3,10 @@
 
 void temp_task(void *duty_cycle_ms);
 
-#define CONFIG_DHT11_PIN GPIO_NUM_22
+#define CONFIG_DHT11_PIN GPIO_NUM_12
 #define CONFIG_CONNECTION_TIMEOUT 5
 
-#define ONEWIRE_BUS_GPIO 12
+#define ONEWIRE_BUS_GPIO 22
 #define ONEWIRE_MAX_DEVS 4
 
 #endif // !TEMPETURE
