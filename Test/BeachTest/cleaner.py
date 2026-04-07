e0f8df2b6900c46bcf47d2f1988b6181e8e77647
zaddmc <davidbk927@gmail.com>
2026-04-07 16:16:40 +0200

    look at that clean csv

--- Test/BeachTest/cleaner.py
+++ Test/BeachTest/cleaner.py
@@ -10,12 +10,19 @@ def clean(filepath: str):
 
     output = []
     reaced_start = False
+    cords = ""
+    ids = ""
     for line in data:
         if not reaced_start:
             if "TXtask: Sent Packet with id: '0'" in line:
                 reaced_start = True
             else:
                 continue
+
+        if "GPS: Latitude:Longitude" in line:
+            cords = line.split("GPS: Latitude:Longitude:  ")[1][:-1]
+            cords = cords.replace(" ", "")
+
         skip = False
         for trash in ["PCNT", "GPS", "bounce"]:
             if trash in line:
@@ -23,7 +30,23 @@ def clean(filepath: str):
                 continue
         if skip:
             continue
-        output.append(ansi_escape.sub("", line))
+        line = ansi_escape.sub("", line)
+        line = line[3:]
+        line = line.replace(") ", ",")
+        time = line.split(",")[0]
+        if "Sent" in line:
+            ids = re.findall(r"id: '(\d+)'", line)[0]
+            line = ",".join([time, "sent", ids, cords]) + "\n"
+        if "Bouncing" in line:
+            ids = re.findall(r"id='(\d+)'", line)[0]
+            line = ",".join([time, "bounced", ids, cords]) + "\n"
+        if "Ignoring" in line:
+            ids = re.findall(r"id='(\d+)'", line)[0]
+            line = ",".join([time, "ignored", ids, cords]) + "\n"
+        if "Unknown" in line:
+            line = ",".join([time, "unknown", ids, cords]) + "\n"
+
+        output.append(line)
 
     with open("clean_" + filepath, "w") as file:
         file.writelines(output)
